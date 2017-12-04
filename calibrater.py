d96af3ae54dfd3a070f2a03e318edfdebebfa01c
TonitruiAula <liu_boqian@sina.com>
2017-12-04 19:56:17 +0800

    calibration(class)

--- calibrater.py
+++ calibrater.py
@@ -11,42 +11,42 @@ class calibrater:
         self.criteria = (cv2.TERM_CRITERIA_MAX_ITER | cv2.TERM_CRITERIA_EPS, 30, 0.001)
         self.numWidth = 10
         self.numHeight = 7
-        self.ret = 0
-        self.mtx = np.zeros((3,3))
-        self.dist = np.zeros((1,3))
-        self.rvecs = np.zeros((1,3))
-        self.tvecs = np.zeros((1,3))
         self.cal = False
     
-    def calCalibrate(self):
+
+    def initCalibration(self):
+        self.numWidth = cv2.getTrackbarPos('棋盘格宽','capture')
+        self.numHeight = cv2.getTrackbarPos('棋盘格高','capture')
+        self.objpoints = []
+        self.imgpoints = []
+        print 'Start calibrating...'
+
+    def shotBoards(self):
         w = self.numWidth - 1
         h = self.numHeight - 1
-
         objp = np.zeros((w*h,3), np.float32)
         objp[:,:2] = np.mgrid[0:w,0:h].T.reshape(-1,2)
-        objpoints = [] # 在世界坐标系中的三维点
-        imgpoints = [] # 在图像平面的二维点
-        gray = cv2.cvtColor(self.frame,cv2.COLOR_BGR2GRAY)
-        ret, corners = cv2.findChessboardCorners(gray, (w,h),None)
+        ret, corners = cv2.findChessboardCorners(self.gray, (w,h),None)
         if ret == True:
-            cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),self.criteria)
-            objpoints.append(objp)
+            cv2.cornerSubPix(self.gray,corners,(11,11),(-1,-1),self.criteria)
+            self.objpoints.append(objp)
             #print u'世界坐标点\n', objpoints
-            imgpoints.append(corners)
+            self.imgpoints.append(corners)
             # 将角点在图像上显示
-            #print u'图像坐标点\n', imgpoints
             img = self.frame.copy()
             cv2.drawChessboardCorners(img, (w,h), corners, ret)
-            #ccv2.imshow('findCorners',img)
-            #cv2.imwrite('corner.png',img)
-            self.ret, self.mtx, self.dist, self.rvecs, self.tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-            # print "ret:",ret  
-            # print "mtx:\n",mtx        # 内参数矩阵  
-            # print "dist:\n",dist      # 畸变系数   distortion cofficients = (k_1,k_2,p_1,p_2,k_3)  
-            # print "rvecs:\n",rvecs    # 旋转向量  # 外参数  
-            # print "tvecs:\n",tvecs    # 平移向量  # 外参数  
+            cv2.imshow('findCorners',img)
+            print 'Picture shot for calibration'
         else:
             print 'Cannot find corners'
+
+    def printResult(self,tr = False):
+        print "ret:",self.ret  
+        print "mtx:\n",self.mtx        # 内参数矩阵  
+        if tr == True:
+            print "dist:\n",self.dist      # 畸变系数   distortion cofficients = (k_1,k_2,p_1,p_2,k_3)  
+            print "rvecs:\n",self.rvecs    # 旋转向量  # 外参数  
+            print "tvecs:\n",self.tvecs    # 平移向量  # 外参数  
 
 
     def calibrate(self):
@@ -63,6 +63,7 @@ class calibrater:
             #cv2.calibrateCamera()
             self.numWidth = cv2.getTrackbarPos('棋盘格宽','capture')
             self.numHeight = cv2.getTrackbarPos('棋盘格高','capture')
+            self.gray = cv2.cvtColor(self.frame,cv2.COLOR_BGR2GRAY)
             key = cv2.waitKey(1) & 0xFF
             if key == ord('q'):
                 self.cal = False
@@ -71,17 +72,17 @@ class calibrater:
             #     print u'棋盘格宽: ', numWidth
             #     print u'棋盘格高: ', numHeight
             elif key == ord('c'):
-                self.cal = True
+                self.cal = not self.cal
                 if self.cal == True:
-                    # print "ret:",self.ret  
-                    print "mtx:\n",self.mtx        # 内参数矩阵  
-                    # print "dist:\n",self.dist      # 畸变系数   distortion cofficients = (k_1,k_2,p_1,p_2,k_3)  
-                    # print "rvecs:\n",self.rvecs    # 旋转向量  # 外参数  
-                    # print "tvecs:\n",self.tvecs    # 平移向量  # 外参数  
-                #self.cal = not self.cal
+                    self.initCalibration()
+                else:
+                    self.ret, self.mtx, self.dist, self.rvecs, self.tvecs = cv2.calibrateCamera(self.objpoints, self.imgpoints, self.gray.shape[::-1], None, None)
+                    self.printResult()
+                    print 'Calibration finished!'
+            elif key == ord('s'):
+                if self.cal == True:
+                    self.shotBoards()
                 
-            if self.cal:
-                self.calCalibrate()
         vc.release()
         cv2.destroyAllWindows()
 
