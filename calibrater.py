240e38c06d9e3e2bedbe15536edbed198f09b464
TonitruiAula <liu_boqian@sina.com>
2017-12-04 20:10:51 +0800

    calibration(class)

--- calibrater.py
+++ calibrater.py
@@ -7,82 +7,92 @@ def fun(x):
     pass
 
 class calibrater:
+    #初始化格的宽高以及标定状态
     def __init__(self):
-        self.criteria = (cv2.TERM_CRITERIA_MAX_ITER | cv2.TERM_CRITERIA_EPS, 30, 0.001)
         self.numWidth = 10
         self.numHeight = 7
         self.cal = False
-    
-
+        
+    #初始化标定
     def initCalibration(self):
+        #获取格的宽高
         self.numWidth = cv2.getTrackbarPos('棋盘格宽','capture')
         self.numHeight = cv2.getTrackbarPos('棋盘格高','capture')
-        self.objpoints = []
-        self.imgpoints = []
+        #清空世界坐标点和图像坐标点
+        self.objpoints = [] #世界坐标点
+        self.imgpoints = [] #图像坐标点
         print 'Start calibrating...'
 
+    #为标定工作拍摄图片
     def shotBoards(self):
+        #获取内部的格点数目
         w = self.numWidth - 1
         h = self.numHeight - 1
+        # 设置寻找亚像素角点的参数，最大循环次数30和最大误差容限0.001
+        criteria = (cv2.TERM_CRITERIA_MAX_ITER | cv2.TERM_CRITERIA_EPS, 30, 0.001)
+        #获取标定板角点的位置
         objp = np.zeros((w*h,3), np.float32)
         objp[:,:2] = np.mgrid[0:w,0:h].T.reshape(-1,2)
         ret, corners = cv2.findChessboardCorners(self.gray, (w,h),None)
         if ret == True:
-            cv2.cornerSubPix(self.gray,corners,(11,11),(-1,-1),self.criteria)
+            #获取亚像素角点
+            cv2.cornerSubPix(self.gray,corners,(11,11),(-1,-1),criteria)
+            #添加世界坐标点和图像坐标点
             self.objpoints.append(objp)
-            #print u'世界坐标点\n', objpoints
             self.imgpoints.append(corners)
             # 将角点在图像上显示
             img = self.frame.copy()
             cv2.drawChessboardCorners(img, (w,h), corners, ret)
             cv2.imshow('findCorners',img)
+
             print 'Picture shot for calibration'
         else:
             print 'Cannot find corners'
 
+    #打印结果，tr表示是否打印外参数
     def printResult(self,tr = False):
         print "ret:",self.ret  
         print "mtx:\n",self.mtx        # 内参数矩阵  
+        print "dist:\n",self.dist      # 畸变系数   distortion cofficients = (k_1,k_2,p_1,p_2,k_3)  
         if tr == True:
-            print "dist:\n",self.dist      # 畸变系数   distortion cofficients = (k_1,k_2,p_1,p_2,k_3)  
             print "rvecs:\n",self.rvecs    # 旋转向量  # 外参数  
             print "tvecs:\n",self.tvecs    # 平移向量  # 外参数  
 
-
+    #标定过程
     def calibrate(self):
+        #获取摄像头信息
         vc = cv2.VideoCapture(0)
-
+        #建立窗口
         cv2.namedWindow('capture')
         cv2.createTrackbar('棋盘格宽','capture',10,30,fun)
         cv2.createTrackbar('棋盘格高','capture',7,30,fun)
 
         while(1):
-            ret,self.frame = vc.read()
-            #print ret
+            ret,self.frame = vc.read()  #读取当前帧
             cv2.imshow('capture',self.frame)
-            #cv2.calibrateCamera()
+            #更新棋盘格宽高
             self.numWidth = cv2.getTrackbarPos('棋盘格宽','capture')
             self.numHeight = cv2.getTrackbarPos('棋盘格高','capture')
+            #计算灰度图
             self.gray = cv2.cvtColor(self.frame,cv2.COLOR_BGR2GRAY)
+            #获取按键
             key = cv2.waitKey(1) & 0xFF
-            if key == ord('q'):
+            if key == ord('q'): #q键退出
                 self.cal = False
                 break
-            # elif key == ord('a'):
-            #     print u'棋盘格宽: ', numWidth
-            #     print u'棋盘格高: ', numHeight
-            elif key == ord('c'):
-                self.cal = not self.cal
+            elif key == ord('c'):   #c键开始/结束标定
+                self.cal = not self.cal #切换状态
+                #开始初始化标定
                 if self.cal == True:
                     self.initCalibration()
+                #结束是计算标定结果并输出
                 else:
                     self.ret, self.mtx, self.dist, self.rvecs, self.tvecs = cv2.calibrateCamera(self.objpoints, self.imgpoints, self.gray.shape[::-1], None, None)
                     self.printResult()
                     print 'Calibration finished!'
-            elif key == ord('s'):
+            elif key == ord('s'):   #s键拍照
                 if self.cal == True:
-                    self.shotBoards()
-                
+                    self.shotBoards()                
         vc.release()
         cv2.destroyAllWindows()
 
