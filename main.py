a5e0130bf16ae86dd85305378fe5da3cedc06e7d
TonitruiAula <liu_boqian@sina.com>
2017-12-06 19:28:41 +0800

    main function

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # -*- coding:utf-8 -*-
 
+import sys
 from tools import *
 from calibrater import calibrater
 
-c = calibrater()
-c.calibrate(0)
-m,d = avgMtx()
-print '\nAverage mtx:\n',m
-print 'Average dist:\n',d
+if __name__ == '__main__':
+    c = calibrater()
+    c.calibrate(0)
+    m,d = avgMtx()
+    print '\nAverage mtx:\n',m
+    print 'Average dist:\n',d
